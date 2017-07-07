588d5f14a778d1353d28d4f1f65438a7d361d68e
G.J. Mertens <G.J.G.J.W.Mertens@uva.nl>
2017-07-07 20:06:27 +0200

    working on database access

--- mybackend/server/api/post/postRoutes.js
+++ mybackend/server/api/post/postRoutes.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
-var controller = require('./userController');
+var controller = require('./postController');
 var createRoutes = require('../../util/createRoutes');
 createRoutes(controller, router);
 
